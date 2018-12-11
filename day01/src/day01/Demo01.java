d463cb3b3612ba9e08a385de95978d277d256c67
gkl <gkl@163.com>
2018-12-11 11:37:45 +0800

    day01

--- day01/src/day01/Demo01.java
+++ day01/src/day01/Demo01.java
@@ -4,6 +4,6 @@ public class Demo01 {
 	public static void main(String[] args) {
 		System.out.println("==========");
 		System.out.println("hello old li!!!");
-		System.out.println("а��");
+		System.out.println("呵呵 邪光");
 	}
 }
